ee51348390fead846a754c9def9a8a6a4b474ed4
anthony28 <anthony@wearea2.com>
2021-01-12 21:31:05 -0500

    minor updates to calc js

---
+++ script.js
@@ -0,0 +1,35 @@
+// DOM elements
+
+const input = document.getElementById("input--field");
+const button1 = document.getElementById("num1");
+const button2 = document.getElementById("num2");
+const button3 = document.getElementById("num3");
+const button4 = document.getElementById("num4");
+const button5 = document.getElementById("num5");
+const button6 = document.getElementById("num6");
+const button7 = document.getElementById("num7");
+const button8 = document.getElementById("num8");
+const button9 = document.getElementById("num9");
+const button0 = document.getElementById("num0");
+const buttonPlus = document.getElementById("opPlus");
+const buttonMinus = document.getElementById("opMinus");
+const buttonEuqals = document.getElementById("opEquals");
+
+input.value = "0";
+
+const displayNumber = function () {
+  button1.textContent;
+};
+button1.addEventListener("click");
+// button2.addEventListener("click", displayNumber);
+// button3.addEventListener("click", displayNumber);
+// button4.addEventListener("click", displayNumber);
+// button5.addEventListener("click", displayNumber(button5));
+// button6.addEventListener("click", displayNumber(button6));
+// button7.addEventListener("click", displayNumber(button7));
+// button8.addEventListener("click", displayNumber(button8));
+// button9.addEventListener("click", displayNumber(button9));
+// button0.addEventListener("click", displayNumber(button0));
+// button2.addEventListener("click", displayNumber(button2));
+// button2.addEventListener("click", displayNumber(button2));
+console.log(button1.textContent);
